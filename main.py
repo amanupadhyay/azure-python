a5eb823bcb3f9af5d2c1295b440b624f77753307
Aman Upadhyay <amanupadhyay@users.noreply.github.com>
2023-11-20 14:29:57 +0000

    database connect disconnect event added

--- main.py
+++ main.py
@@ -34,6 +34,15 @@ engine = sqlalchemy.create_engine(
 
 metadata.create_all(engine)
 
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
 @app.get("/home")
 def home():
     print("")
