d702fa056ccac9b14d0c7496c4e7a8f14d7534c2
Aman Upadhyay <amanupadhyay@users.noreply.github.com>
2023-11-20 13:35:12 +0000

    added APIs and DB

--- main.py
+++ main.py
@@ -1,7 +1,52 @@
 from fastapi import FastAPI, status
+import sqlalchemy
+import databases
+import os
+import urllib
+
+host_server = os.environ.get('host_server', 'localhost')
+db_server_port = urllib.parse.quote_plus(str(os.environ.get('db_server_port', '5432')))
+database_name = os.environ.get('database_name', 'fastapi')
+db_username = urllib.parse.quote_plus(str(os.environ.get('db_username', 'postgres')))
+db_password = urllib.parse.quote_plus(str(os.environ.get('db_password', 'secret')))
+ssl_mode = urllib.parse.quote_plus(str(os.environ.get('ssl_mode','prefer')))
+DATABASE_URL = 'postgresql://{}:{}@{}:{}/{}?sslmode={}'.format(db_username, db_password, host_server, db_server_port, database_name, ssl_mode)
+
+database = databases.Database(DATABASE_URL)
+
+metadata = sqlalchemy.MetaData()
+
 
 app = FastAPI()
+
+
+notes = sqlalchemy.Table(
+    "todo",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("text", sqlalchemy.String),
+    sqlalchemy.Column("completed", sqlalchemy.Boolean),
+)
+engine = sqlalchemy.create_engine(
+    #DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL, pool_size=3, max_overflow=0
+)
+
+metadata.create_all(engine)
 
 @app.get("/home")
 def home():
     return {"message":"Hello World"}
+
+
+@app.get("/insert-sample-data")
+async def insert():
+    query = notes.insert().values(text="Sample notes", completed=False)
+    last_record_id = await database.execute(query)
+    return {"success":True, "id": last_record_id}
+
+
+@app.get("/notes/", status_code = status.HTTP_200_OK)
+async def read_notes(skip: int = 0, take: int = 20):
+    query = notes.select().offset(skip).limit(take)
+    return await database.fetch_all(query)
